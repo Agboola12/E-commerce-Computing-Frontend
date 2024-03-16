38eed3fcf6edc07e62b1fba91077fd39ad0696b8
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 18:32:54 +0100

    wip on the product

--- src/user/Product.jsx
+++ src/user/Product.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import NavBar from './NavBar'
 import style from '../styles/Style.module.css'
+import product from '../assets/product.png'
 
 
 const Product = () => {
-  return (
-    <div>
-        <NavBar/>
+    return (
+        <div>
+            <NavBar />
 
-        <header id={style.head}>
+            <header id={style.head}>
                 <p className='fw-bold fs-1 text-white text-center'>About Us</p>
             </header>
-    </div>
-  )
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-3'>
+                        <div className='shadow-sm bg-white'>
+                            <img src={product} />
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Product
