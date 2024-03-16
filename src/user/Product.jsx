263e9045de5206c1732ccd9c6030cdd02b74f9eb
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 18:54:20 +0100

    wip on the product

--- src/user/Product.jsx
+++ src/user/Product.jsx
@@ -13,11 +13,38 @@ const Product = () => {
                 <p className='fw-bold fs-1 text-white text-center'>About Us</p>
             </header>
 
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-3'>
-                        <div className='shadow-sm bg-white'>
-                            <img src={product} />
+            <div className='container mt-5'>
+                <div className='row text-center mb-5 '>
+                    <div className='col-lg-3 shadow bg-white rounded-sm  '>
+                        <div className=''>
+                            <img src={product} className='w-100' />
+                            <p className='text-primary fw-bold mt-2'>Dell</p>
+                            <p className='fw-bold'>$45.00</p>
+                            <button className='btn btn-success text-white mb-3'>View Button</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-3 shadow bg-white rounded-sm  '>
+                        <div className=''>
+                            <img src={product} className='w-100' />
+                            <p className='text-primary fw-bold mt-2'>Dell</p>
+                            <p className='fw-bold'>$45.00</p>
+                            <button className='btn btn-success text-white mb-3'>View Button</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-3 shadow bg-white rounded-sm  '>
+                        <div className=''>
+                            <img src={product} className='w-100' />
+                            <p className='text-primary fw-bold mt-2'>Dell</p>
+                            <p className='fw-bold'>$45.00</p>
+                            <button className='btn btn-success text-white mb-3'>View Button</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-3 shadow bg-white rounded-sm  '>
+                        <div className=''>
+                            <img src={product} className='w-100' />
+                            <p className='text-primary fw-bold mt-2'>Dell</p>
+                            <p className='fw-bold'>$45.00</p>
+                            <button className='btn btn-success text-white mb-3'>View Button</button>
                         </div>
                     </div>
 
