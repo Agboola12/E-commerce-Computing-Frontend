7dd788cfe84c6f71640979e5681bf9137af88790
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 16:53:05 +0100

    wip on the about us

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import Home from "./user/Home";
+import AboutUs from "./user/AboutUs";
 
 const Routing = () => {
   return (
         <>
             <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<AboutUs/>}/>
             </Routes>
     </>
   );
---
+++ src/user/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <NavBar/>
+
+        <header className={style.head}>
+
+        </header>
+    </div>
+  )
+}
+
+export default AboutUs
