966d64c2659809432b734e4cbca3312df08787f9
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 16:05:56 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Routing from './Routing'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <Routing />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
