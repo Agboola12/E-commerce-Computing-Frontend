6f7e0a4eb41df07d045f0e16b88d9b3980d2cc9e
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 18:55:55 +0100

    wip on the product

--- src/user/AboutUs.jsx
+++ src/user/AboutUs.jsx
@@ -55,32 +55,7 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            <div className='container-fluid p-5' id={style.footer}>
-            <div className='row'>
-                <div className='col-lg-3'>
-                    <p>
-                        <i className='fa fa-time'></i><br/>
-                        -24/7 active
-                         
-                    </p>
-                </div>
-                <div className='col-lg-3'>
-                    <p>
-                        <i className='fa fa-location-dot'></i><br/>
-                        103 North Loundon Street, <br/>
-                        Winchider, VA 
-                    </p>
-                </div>
-                <div className='col-lg-3'>
-                    <p>
-                        Help
-                        103 North Loundon Street, <br/>
-                        Winchider, VA 
-                    </p>
-                </div>
 
-            </div>
-            </div>
 
         </div>
     )
