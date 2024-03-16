dc89b5f04c55af685c9fc1ed90a16d2db69018e4
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-16 16:10:55 +0100

    setting up the navbar

--- src/user/NavBar.jsx
+++ src/user/NavBar.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+                  <nav className="navbar navbar-expand-sm navbar-light " id="home" >
+
+<a href="" className="navbar-brand p-3">
+  <img src={logo} alt='jjkbd' className='w-50' />
+</a>
+<button className="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="sab">
+  <ul className="navbar-nav list-line mx-auto">
+    <li className="nav-item active list-line-item mr-5 ">
+      <Link to="/dashboard" className="nav-link active text-center ">Dashboard</Link>
+    </li>
+    <li className="nav-item dropdown list-line-item mr-5 " >
+      <Link to="/document" className="nav-link active text-center">Document </Link>
+    </li>
+
+  </ul>
+  <ul className="navbar-nav list-line ml-auto">
+    <li className="nav-item active list-line-item mr-5 " >
+      <li onClick={LogOut} className='text-light nav-link text-center btn btn' style={{ backgroundColor: '#FF9500' }} >Logout</li>
+    </li>
+  </ul>
+
+</div>
+</nav>
+    </div>
   )
 }
 
