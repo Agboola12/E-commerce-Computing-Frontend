e4cb16518006971cf86a1a741b8c5c1dfdf762ce
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-17 21:43:10 +0100

    wip on the project

--- src/user/Footer.jsx
+++ src/user/Footer.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import style from '../styles/Style.module.css'
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import style from '../styles/Style.module.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
+    const handleMobileClick = (mobile) => {
+        window.location.href = `tel:${mobile}`;
+    };
 
+    const handleEmailClick = (email) => {
+        window.location.href = `mailto:${email}`;
+    };
+
+    return (
         <div className='container-fluid mx-auto text-center p-5' id={style.footer}>
             <div className='row '>
                 <div className='col-lg-3'>
                     <p>
                         <i className='fa fa-clock'></i><br />
-                        -24/7 active
-
+                        - 24/7 active
                     </p>
                 </div>
                 <div className='col-lg-3'>
@@ -25,22 +30,20 @@ const Footer = () => {
                 <div className='col-lg-3'>
                     <h6 className="text-medium text-uppercase text-white"><i className='fa fa-envelope'></i></h6>
                     <ul className="list-unstyled">
-                        <li><Link to="mailto:admin@victorycenter.org.uk" className="text-white text-decoration-none">admin@victorycenter.org.uk</Link></li>
-                        <li><Link to="mailto:pastordoherty@victorycenter.org.uk" className="text-white text-decoration-none">pastordoherty@victorycenter.org.uk</Link></li>
+                        <li className='text-white text-decoration-none' onClick={() => handleEmailClick('admin@victorycenter.org.uk')}>admin@victorycenter.org.uk</li>
+                        <li className='text-white text-decoration-none' onClick={() => handleEmailClick('pastordoherty@victorycenter.org.uk')}>pastordoherty@victorycenter.org.uk</li>
                     </ul>
                 </div>
                 <div className='col-lg-3'>
                     <h6 className="text-medium text-uppercase text-white"><i className='fa fa-phone'></i></h6>
                     <ul className="list-unstyled">
-                        <li><Link to="mailto:admin@victorycenter.org.uk" className="text-white text-decoration-none">admin@victorycenter.org.uk</Link></li>
-                        <li onClick={() => handleMobileClick}> +2349070684164 </li>
+                        <li className='text-white text-decoration-none' onClick={() => handleMobileClick('+2349070684164')}>+2349070684164</li>
+                        <li className='text-white text-decoration-none' onClick={() => handleMobileClick('+2349070684164')}>+2349070684164</li>
                     </ul>
                 </div>
-
             </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default Footer
+export default Footer;
