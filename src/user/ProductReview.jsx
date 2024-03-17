6039996d145448219a9930ee9bde62a2c90b173b
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-18 00:51:42 +0100

    wip on the project

--- src/user/ProductReview.jsx
+++ src/user/ProductReview.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar'
+import style from '../styles/Style.module.css'
+
 
 const ProductReview = () => {
   return (
@@ -7,7 +9,7 @@ const ProductReview = () => {
         <NavBar/>
 
         <header id={style.head}>
-                <p className='fw-bold fs-1 text-white text-center' style={{ marginTop: '2em' }}>About Us</p>
+                <p className='fw-bold fs-1 text-white text-center' style={{ marginTop: '2em' }}>Product Review</p>
             </header>
     </div>
   )
