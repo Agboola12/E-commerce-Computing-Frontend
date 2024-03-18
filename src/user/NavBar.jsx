89e204c007622eb6da0cfda069a60bf656d0def7
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-18 12:26:08 +0100

    wip on the project

--- src/user/NavBar.jsx
+++ src/user/NavBar.jsx
@@ -5,34 +5,6 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            {/* <nav className="navbar navbar-expand-sm navbar-light bg-white" id="home" >
-                <a href="" className="navbar-brand">
-                    <img src={logo} alt='jjkbd'  width='100px' className='ml-5'/>
-                    </a>
-                    <button className="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="sab">
-                    <ul className="navbar-nav list-line mx-auto text-center fw-bold fs-5">
-                    <li className="nav-item active list-line-item mr-5 ">
-                    <Link to="/dashboard" className="nav-link active text-center ">Home</Link>
-                        </li>
-                        <li className="nav-item dropdown list-line-item mr-5 " >
-                        <Link to="/product" className="nav-link active text-center">Our Products </Link>
-                        </li>
-                        <li className="nav-item active list-line-item mr-5 ">
-                        <Link to="/about" className="nav-link active text-center ">About Us</Link>
-                        </li>
-                        <li className="nav-item dropdown list-line-item mr-5 " >
-                        <Link to="/document" className="nav-link active text-center">Contact </Link>
-                        </li>
-                        <li className="nav-item dropdown list-line-item mr-5 " >
-                        <Link to="/review" className="nav-link active text-center">Product Review </Link>
-                        </li>
-                        </ul>
-                        </div>
-                    </nav> */}
-
             <nav className="navbar navbar-expand-lg navbar-light bg-white">
                 <img src={logo} alt='jjkbd' width='80px' className='ml-5' />
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
